a7ab44d131e3b8c8f48765acfb0cff04a789d46f
stebru <stefan@colours.no>
2014-07-10 15:42:35 +0200

    Try it 2

--- grunt/qunit.js
+++ grunt/qunit.js
@@ -1,14 +1,7 @@
 // https://www.npmjs.org/package/grunt-contrib-qunit
 
 module.exports = {
-    test: {
-        // options: {
-        //     urls: [
-        //         'http://127.0.0.1:8080/tests/tests.html',
-        //     ]
-        // },
-        files: [
-            'tests/tests.html'
-        ]
-    }
+    files: [
+        'tests/tests.html'
+    ]
 };
