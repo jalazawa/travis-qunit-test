c0ba0932ffc44c4e6ccd1c76ec81123eb2978556
stebru <stefan@colours.no>
2014-07-10 15:40:06 +0200

    Try it

--- grunt/qunit.js
+++ grunt/qunit.js
@@ -2,13 +2,13 @@
 
 module.exports = {
     test: {
-        options: {
-            urls: [
-                'http://127.0.0.1:8080/tests/tests.html',
-            ]
-        },
-        all: [
-            'http://127.0.0.1:8080/tests/tests.html'
+        // options: {
+        //     urls: [
+        //         'http://127.0.0.1:8080/tests/tests.html',
+        //     ]
+        // },
+        files: [
+            'tests/tests.html'
         ]
     }
 };
